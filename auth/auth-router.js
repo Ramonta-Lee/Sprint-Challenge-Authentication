686ffd4d57f423b0a6f7752fdf430cf654b0806f
unknown <viola4lfe@gmail.com>
2020-02-28 11:12:04 -0500

    initial commit; added bcrypt

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,7 +1,16 @@
+const bcrypt = require("bcryptjs");
+
 const router = require('express').Router();
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+
+  const hash = bcrypt.hashSync(user.password, 12);
+
+  user.password = hash;
+
+  
 });
 
 router.post('/login', (req, res) => {
